f6d9954da49045f28fc5a8c3562f2b23803439a7
esolar07 <esolar07@gmail.com>
2016-10-04 15:19:08 -0400

    working on fixing issue with foundation js

--- front-page.php
+++ front-page.php
@@ -1,4 +1,29 @@
+<?php
+  /*
+    Template Name: Home Page
+  */
+ ?>
+
 <?php get_header(); ?>
+
+<div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
+  <ul class="orbit-container">
+    <button class="orbit-previous" aria-label="previous"><span class="show-for-sr">Previous Slide</span>&#9664;</button>
+    <button class="orbit-next" aria-label="next"><span class="show-for-sr">Next Slide</span>&#9654;</button>
+    <li class="orbit-slide is-active">
+      <img class="orbit-image" src="http://placehold.it/2000x750&text=1st">
+    </li>
+    <li class="orbit-slide">
+      <img class="orbit-image" src="http://placehold.it/2000x750&text=2nd">
+    </li>
+    <li class="orbit-slide">
+      <img class="orbit-image" src="http://placehold.it/2000x750&text=3nd">
+    </li>
+    <li class="orbit-slide">
+      <img class="orbit-image" src="http://placehold.it/2000x750&text=4nd">
+    </li>
+  </ul>
+</div>
 
 <div class="row column text-center">
 
--- functions.php
+++ functions.php
@@ -14,18 +14,19 @@ function register_theme_menus(){
 add_action('init', 'register_theme_menus');
 
 function candles_theme_styles(){
-  wp_enqueue_style('foundation_css', get_template_directory_uri().'/style.css');
-  wp_enqueue_style('main_css', 'https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.css');
+  wp_enqueue_style('foundation_css', 'https://cdn.jsdelivr.net/foundation/6.2.3/foundation.min.css');
+  wp_enqueue_style('main_css', get_template_directory_uri().'/style.css');
+
 }
 // adding css
 add_action('wp_enqueue_scripts', 'candles_theme_styles');
 
 function candles_theme_js(){
   wp_enqueue_script('modernizr_js', get_template_directory_uri() . "/js/modernizr.js", '', '', false);
-  wp_enqueue_script('foundation_js', get_template_directory_uri() . "/js/foundation.js", array('jquery'), '', true);
+  wp_enqueue_script('foundation_js',get_template_directory_uri() . "/js/foundation.min.js", array('jquery'), '', true);
   wp_enqueue_script('main_js', get_template_directory_uri() . "/js/app.js", array('jquery', 'foundation_js'), '', true);
 }
 // adding js
-add_action('wp_enqeue_scripts', 'candles_themes_js');
+add_action('wp_enqueue_scripts', 'candles_theme_js');
 
 ?>
